1e69cab7a2adab60af361fc8a4544b83a225bd74
samuelpinheiro <samuel.pinheiro.senai564@gmail.com>
2023-08-07 14:35:03 -0300

    fied:created edit post

--- assets/js/index-scrip.js
+++ assets/js/index-scrip.js
@@ -1,21 +1,53 @@
 const posts = [];
-function savePost(){
-    const title = document.getElementById("").value;
-    const resumo = document.getElementById("").value;
-    const publisher = document.getElementById("").value;
-    const date = document.getElementById("").value;
+function savePost() {
+    const title = document.getElementById("title").value;
+    const resumo = document.getElementById("resumo").value;
+    const publisher = document.getElementById("publisher").value;
+    const date = document.getElementById("date").value;
 
-    if(title && resume && publisher && date){
+    console.log(title, resumo, publisher, date)
+    if (title && resumo && publisher && date) {
         storePost(title, resumo, publisher, date);
+        cleanFields();
     }
 }
-function  storePost(title, resumo, publisher, date){
-const post = {
-    title: title ,
-    resumo: resumo ,
-    publisher: publisher,
-    date: date
-};
+function storePost(title, resumo, publisher, date) {
+    const post = {
+         title,
+     resumo,
+    publisher,
+         date
+    };
 
-posts.push(post)
+    posts.push(post);
+    console.log(post);
+    showPosts()
+}
+function showPosts() {
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+              <div  class="post">
+               <h2>${post.title}</h2>
+               <p><strong>Resumo:</strong> ${post.resumo}</p>
+               <p><strong>Autor:</strong> ${post.publisher}</p>
+               <p><strong>Data de Publicaçâo:</strong> ${post.date}</p>
+
+               <button onclick="editPost(${index})">Editar</button>
+               <button onclick="removePost(${index}>Remover</button>
+              </div>
+          `;
+    });
+    /* calback e forEach para formar uma eurofunction*/
+    document.getElementById("list").innerHTML = showContent;
+}
+function cleanFields(){
+    document.getElementById("title").value = " ";
+    document.getElementById("resumo").value = " ";
+    document.getElementById("publisher").value = " ";
+    document.getElementById("date").value = " ";
+}
+function editPost(index){
+ const post = posts[index];
 }
