5d89861c473db14dccafd2822b8cafd8da8edb2e
samuelpinheiro <samuel.pinheiro.senai564@gmail.com>
2023-08-07 15:02:45 -0300

    fead: create edit and remove posts functions

--- assets/js/index-scrip.js
+++ assets/js/index-scrip.js
@@ -1,22 +1,38 @@
 const posts = [];
+let indexPost = -1;
 function savePost() {
     const title = document.getElementById("title").value;
     const resumo = document.getElementById("resumo").value;
     const publisher = document.getElementById("publisher").value;
     const date = document.getElementById("date").value;
 
-    console.log(title, resumo, publisher, date)
-    if (title && resumo && publisher && date) {
-        storePost(title, resumo, publisher, date);
-        cleanFields();
+    if (indexPost == -1) {
+        console.log(title, resumo, publisher, date)
+        if (title && resumo && publisher && date) {
+            storePost(title, resumo, publisher, date);
+            cleanFields();
+        } else {
+            if (title && resumo && publisher && date) {
+                posts[indexPost] = {
+                    title,
+                    resumo,
+                    publisher,
+                    date
+                };
+                showPosts();
+                indexPost = -1;
+                cleanFields();
+            }
+        }
     }
+
 }
 function storePost(title, resumo, publisher, date) {
     const post = {
-         title,
-     resumo,
-    publisher,
-         date
+        title,
+        resumo,
+        publisher,
+        date
     };
 
     posts.push(post);
@@ -42,12 +58,23 @@ function showPosts() {
     /* calback e forEach para formar uma eurofunction*/
     document.getElementById("list").innerHTML = showContent;
 }
-function cleanFields(){
-    document.getElementById("title").value = " ";
-    document.getElementById("resumo").value = " ";
-    document.getElementById("publisher").value = " ";
-    document.getElementById("date").value = " ";
+function cleanFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("resumo").value = "";
+    document.getElementById("publisher").value = "";
+    document.getElementById("date").value = "";
 }
-function editPost(index){
- const post = posts[index];
+function editPost(index) {
+    indexPost= index;
+    const post = posts[index];
+
+    document.getElementById("title").value = post.title;
+    document.getElementById("resumo").value = post.resumo;
+    document.getElementById("publisher").value = post.publisher;
+    document.getElementById("date").value = post.date;
+}
+function removePost (index){
+    posts.splice(index, 1);
+
+    showPosts()
 }
